939be648c1c62780559f78006b0d6c8c86537a28
isisanchalee <isis.wenger@onelogin.com>
2015-11-29 15:40:04 -0800

    Add sessions controller specs

--- spec/features/sessions_controller_spec.rb
+++ spec/features/sessions_controller_spec.rb
@@ -1,6 +1,44 @@
 require 'rails_helper'
 
 RSpec.feature 'Sessions Controller', :type => :feature do
+  describe 'the signup process' do 
+
+    scenario 'User signs up without email' do
+      visit 'users/sign_up'
+      fill_in 'email-field', with: ''
+      fill_in 'pw-field', with: 'testing'
+      click_button 'Sign up'
+      expect(page).to have_content 'Email can\'t be blank'
+    end
+
+    scenario 'User signs in with invalid email' do
+      visit 'users/login'
+      fill_in 'email', with: 'butts'
+      fill_in 'password', with: 'password'
+      click_button 'Log in'
+      expect(page).to have_content 'Sign up'
+    end
+
+    scenario 'With valid credentials' do
+      visit 'users/sign_up'
+      fill_in 'email-field', with: 'testing@test.test'
+      fill_in 'pw-field', with: 'test'
+      click_button 'Sign up'
+      expect(page).to have_content 'You have signed up successfully'
+    end
+
+    scenario 'New user signs out' do
+      visit 'users/sign_up'
+      fill_in 'email-field', with: 'testing@test.test'
+      fill_in 'pw-field', with: 'test'
+      click_button 'Sign up'
+      expect(page).to have_content 'You have signed up successfully'
+      click_link 'Sign Out'
+      expect(page).to have_content 'Sign In'
+    end
+  end
+
+
   describe 'the signin process' do
     let(:user){ FactoryGirl.create(:user) }
    
@@ -8,10 +46,8 @@ RSpec.feature 'Sessions Controller', :type => :feature do
       visit 'users/login'
       fill_in 'email', with: user.email
       fill_in 'password', with: user.password
-      byebug
       click_button 'Log in'
-      # expect(user.persisted?).to be true
-      expect(page).to have_content 'CREATE NEW GAME'
+      expect(user.persisted?).to be true
     end
 
     scenario 'User signs in with invalid email' do
@@ -20,16 +56,6 @@ RSpec.feature 'Sessions Controller', :type => :feature do
       fill_in 'password', with: 'password'
       click_button 'Log in'
       expect(page).to have_content 'Sign up'
-    end
-
-    scenario 'User signs out' do
-      visit 'users/login'
-      fill_in 'email', with: user.email
-      fill_in 'password', with: user.password
-      click_button 'Log in'
-      save_and_open_page
-      click_link 'Sign Out'
-      expect(page).to have_content 'Sign In'
     end
   end
 end
