33771980748daa727d95e25f6162a72fe4a76861
isisanchalee <isis.wenger@onelogin.com>
2015-11-30 12:12:26 -0800

    Clean up API Gamescontroller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -3,19 +3,7 @@ module Api
     class GamesController < Api::V1::ApiController 
       # before_action :ensure_players_belong_in_game, only:[ :show, :update ]
       skip_before_action :authenticate_user!, only: [:update]
-      
-      after_action :setup_boards, only: :update
-
-      # create boards and change game state
-      # after second official player found
-      def update
-        @game = Game.find(params[:id])
-        handle_errors
-        @game.second_player_id = current_user.id 
-        @game.state = 'setup'
-        @game.save! ? join_game : render_game_errors
-      end
-
+    
       # used to trigger board status change
       def update_status
         @game = Game.find(params[:id])
@@ -24,38 +12,10 @@ module Api
       end
 
       private
-      def setup_boards
-        @game.player_ids.each do |player_id|
-          Services::BoardFactory.new({ game_id: @game.id,
-                                       user_id: player_id }).build_board
-        end
-      end
 
       def feedback_factory(msg, status = :ok, start_game = true, custom = '')
         Services::RenderFeedack.new({ msg: msg, status: status,
                               start_game: start_game, custom: custom }).render_message
-      end
-
-      def handle_errors
-        message = :cannot_join if @game.first_player_id == @game.second_player_id
-        message = :game_is_full if @game.is_full?
-        feedback_factory(message).render_message
-      end
-
-      def join_game
-        feedback_factory(:join_game).render_message
-      end
-
-      def render_game_errors
-        feedback_factory(msg: '', :ok, true, custom: @game.errors.full_messages).render_message
-      end
-
-      def ensure_players_belong_in_game
-        render_401 unless @game && @game.player_ids.include?(params[:user_id])
-      end
-
-      def render_401
-        feedback_factory(msg: :unauthorized, :unauthorized, false).render_message
       end
     end
   end
